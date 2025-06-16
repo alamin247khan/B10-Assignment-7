def820343bc957b249ed6e247119ea4b80a16212
Al Amin Khan <alamin24.7khan@outlook.com>
2025-06-16 06:51:00 -0700

    complite

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Players from './components/Players'
 import Newsletter from './components/Newsletter'
+import Footer from './components/Footer'
 
 function App() {
   const [coin, setCoin] = useState(0);
@@ -26,9 +27,13 @@ function App() {
         <Players coin={coin} setCoin={setCoin} selectedPlayers={selectedPlayers} setSelectedPlayers={setSelectedPlayers} />
       </div>
       <div className="mt-5 sm:mt-3">
-        <Newsletter />
+        <div className="flex items-center justify-center bg-transparent mb-16">
+          <Newsletter />
+        </div>
       </div>
-      
+      <div className="mt-5 sm:mt-3">
+        <Footer />
+      </div>
     </>
   )
 }
